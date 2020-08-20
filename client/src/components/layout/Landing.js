e55315baf299fb95a601698498556667312682d6
bob <golusherawat@gmail.com>
2020-08-21 00:09:12 +0530

    test button removed

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -23,7 +23,6 @@ const Landing=({isAuthenticated,loadUser})=>{
           <div className="buttons">
             <Link to="/register" className="btn btn-primary">Sign Up</Link>
             <Link to="/login" className="btn btn-light">Login</Link>
-            <Link to="#" className="btn btn-light" onClick={handleClick}>Test</Link>
           </div>
         </div>
       </div>
